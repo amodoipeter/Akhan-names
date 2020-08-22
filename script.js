0799daaa9ab2d57aaa5c0277b9949ec104309331
amodoipeter <amodoipeter@gmail.com>
2020-08-22 20:58:37 +0300

    Declared birthday, new date of birth, day of the week and gender variables

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-  var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var myBirthday = document.getElementById("myBirthDate").value;
+var dateOfBirth = new Date(myBirthday);
+var dayOfTheWeek = dateOfBirth.getDay();
+var myGender = document.getElementsByName("gender");
