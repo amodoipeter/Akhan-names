45e52cf02f86059125e7e666709c1cd36fecf996
amodoipeter <amodoipeter@gmail.com>
2020-08-22 21:31:21 +0300

    Added an if-else statement

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ var myBirthday = document.getElementById("myBirthDate").value;
 var dateOfBirth = new Date(myBirthday);
 var dayOfTheWeek = dateOfBirth.getDay();
 var myGender = document.getElementsByName("gender");
+if(myBirthday === ""){
+        document.getElementById('message').innerHTML = "<div class=\"alert\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\">x</button>Invalid Date!</div>";
+    }
+    else {
+        for(var i=0;i<myGender.length;i++){
+            if(myGender[i].checked){
+                if(myGender[i].value === "Male"){
+                  document.getElementById('message').innerHTML = "Born on <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name is <span>" + maleAkanNames[dayOfTheWeek] + "</span>";
+                }
+                else {
+                    document.getElementById('message').innerHTML = "Born on <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name is <span>" + femaleAkanNames[dayOfTheWeek] + "</span>";
+                }
+                break;
+            }
+            else {
+                document.getElementById('message').innerHTML = "<div class=\"alert\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\">x</button>Invalid gender!</div>";
+            }
+        }
+    }
+}
