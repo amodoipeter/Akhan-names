11cc16b9a799fab19482d21305601d23686c1e4b
amodoipeter <amodoipeter@gmail.com>
2020-08-22 21:42:23 +0300

    Added functions to get and display the message

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function getAkanName(){
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
@@ -24,4 +25,8 @@ if(myBirthday === ""){
             }
         }
     }
+}
+
+function clearAkanMessage(){
+    document.getElementById('message').innerHTML = "";
 }
